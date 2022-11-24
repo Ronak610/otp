f5679afa3c77640a4c276676b235b75a8599c6bb
mohitzalavadiya <mzpatel14@gmail.com>
2022-11-24 11:51:56 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:otp/otp.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Otp(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
